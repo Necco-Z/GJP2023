5398c47e4c6ada2f25e52f70aba4c6aef21cbc0f
Natan Maia <natanmaiaplays@outlook.com>
2024-01-19 00:30:13 -0300

    tela de titulo preparada pra tela de opções

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -13,5 +13,12 @@ func _on_start_pressed() -> void:
 
 
 func _on_exit_pressed() -> void:
+	for button in $Buttons.get_children():
+		button.disabled = true
+	
 	await Fader.fade_out()
 	get_tree().quit()
+
+
+func _on_options_pressed():
+	Loader.load_scene("res://scenes/menus/options_menu.tscn")
